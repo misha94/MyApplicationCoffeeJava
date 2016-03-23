71252f3f34924d0f9e02a80714bf520eaceb5e9b
misha94 <mishapopkov@yandex.ru>
2016-03-23 22:01:03 +0300

    Just Commit

--- app/src/main/java/com/example/android/myapplicationjava/MainActivity.java
+++ app/src/main/java/com/example/android/myapplicationjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.myapplicationjava;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -13,27 +14,34 @@ import java.text.NumberFormat;
 public class MainActivity extends ActionBarActivity {
 
     int quantity = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /**
-     * This method is called when the order button is clicked.
-     */
+
     public void increment(View view) {
-        quantity = quantity+1;
-        displayQuantity(quantity);
-    }
-    public void decrement(View view) {
-        quantity = quantity-1;
+        quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
     public void submitOrder(View view) {
-            int price = calculatePrice();
-            String priceMessage = createOrderSummary(price);
+
+        CheckBox toppingCheckBoxCream = (CheckBox) findViewById(R.id.toppingCheckBoxCream);
+        boolean toppingCheckedCream = toppingCheckBoxCream.isChecked();
+
+        CheckBox toppingCheckBoxChocolate = (CheckBox) findViewById(R.id.toppingCheckBoxChocolate);
+        boolean toopingCheckedChocolate = toppingCheckBoxChocolate.isChecked();
+
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, toppingCheckedCream, toopingCheckedChocolate);
         displayMessage(priceMessage);
     }
 
@@ -45,8 +53,19 @@ public class MainActivity extends ActionBarActivity {
         return price;
     }
 
-    private String createOrderSummary(int price) {
-        String orderMassage = "Name: Popkov Mikhail"+ "Топпинг" + "\nQuantity: " + quantity + "\nTotal: " + price + " руб." + "\nThank you!";
+    /**
+     * Create summary of the order.
+     *
+     * @param toppingCream is whether or not the user wants whipped cream topping
+     * @param toppingChocolate is whether or not the user wants whipped cream topping
+     * @param price of the order
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean toppingCream, boolean toppingChocolate) {
+
+        String orderMassage = "Name: Popkov Mikhail" + "\nДобаваить взбитые сливки? " + toppingCream;
+        orderMassage += "\nДобавить шоколад? " + toppingChocolate;
+        orderMassage += "\nQuantity: " + quantity + "\nTotal: " + price + " руб." + "\nThank you!";
         return orderMassage;
     }
 
@@ -57,6 +76,7 @@ public class MainActivity extends ActionBarActivity {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -64,4 +84,5 @@ public class MainActivity extends ActionBarActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
 }
